7efa514c5442bde8d8b6f98b5f0a4c1547cc2ae5
massiliasreign <masonvongsavath619@gmail.com>
2026-01-02 17:06:56 -0800

    Read file

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+        return file_contents
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+main()

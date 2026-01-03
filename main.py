33cd0d0dce2c0b8814367a73ce1f8611912d3b9d
massiliasreign <masonvongsavath619@gmail.com>
2026-01-02 18:48:05 -0800

    test

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import sys
-if len(sys.argv) != 2:
-    print("Usage: python3 main.py <path_to_book>")
-    sys.exit(1)
-book_path = sys.argv[1]
-from stats import get_num_words, sorted_letter_count 
-print("============ BOOKBOT ============")
-print("Analyzing book found at books/...")
-print("----------- Word Count ----------")
-get_num_words()
-print("--------- Character Count -------")
-chars = sorted_letter_count()
-for item in chars:
-    print(f"{item['char']}: {item['num']}")
-print("============= END ===============")
+from stats import get_num_words, sorted_letter_count
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    text = get_book_text(book_path)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    num_words = get_num_words(text)
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    chars = sorted_letter_count(text)
+    for item in chars:
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
+
+
+main()
--- stats.py
+++ stats.py
@@ -2,13 +2,11 @@ def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
-def get_num_words():
-    text = get_book_text(book_path)
-    text = text.split()
-    num_words = len(text)
-    print(f"Found {num_words} total words")
-def letter_count():
-    text = get_book_text(book_path)
+def get_num_words(text):
+    words = text.split()
+    num_words = len(words)
+    return num_words
+def letter_count(text):
     letter_counts = {}
     for letter in text:
         if not letter.isalpha():
@@ -24,8 +22,8 @@ def letter_count():
 def sort_on(item):
     return item["num"]
 
-def sorted_letter_count():
-    letter_counts = letter_count()
+def sorted_letter_count(text):
+    letter_counts = letter_count(text)
     sorted_list = []
 
     for char, count in letter_counts.items():
@@ -35,4 +33,3 @@ def sorted_letter_count():
         })
     sorted_list.sort(reverse=True, key=sort_on)
     return sorted_list
-

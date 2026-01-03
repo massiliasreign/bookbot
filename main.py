8103ff7a943f6bbde4004342cc2c277db4f3472f
massiliasreign <masonvongsavath619@gmail.com>
2026-01-02 17:43:45 -0800

    count characters

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-#def get_book_text(filepath):
-   # with open(filepath) as f:
-  #      file_contents = f.read()
- #       return file_contents
-#def main():
-   # book_path = "books/frankenstein.txt"
-  #  text = get_book_text(book_path)
- #   print(text)
-#main()
 from stats import get_num_words 
 get_num_words()
+from stats import letter_count
+letter_count()
--- stats.py
+++ stats.py
@@ -8,3 +8,15 @@ def get_num_words():
     text = text.split()
     num_words = len(text)
     print(f"Found {num_words} total words")
+def letter_count():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    letter_counts = {}
+    for letter in text:
+        letter = letter.lower()
+        if letter not in letter_counts:
+            letter_counts[letter] = 1
+        else:
+            letter_counts[letter] += 1
+    
+    print(letter_counts)

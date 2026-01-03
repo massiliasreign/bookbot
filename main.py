1fd379d26b89e62e09cc27ff0839d711f990b370
massiliasreign <masonvongsavath619@gmail.com>
2026-01-02 17:15:38 -0800

    finding words

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+#def get_book_text(filepath):
+   # with open(filepath) as f:
+  #      file_contents = f.read()
+ #       return file_contents
+#def main():
+   # book_path = "books/frankenstein.txt"
+  #  text = get_book_text(book_path)
+ #   print(text)
+#main()
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
@@ -5,5 +14,7 @@ def get_book_text(filepath):
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    text = text.split()
+    num_words = len(text)
+    print(f"Found {num_words} total words")
 main()

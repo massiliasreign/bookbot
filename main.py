473ee988ac02588ac168d3c323ebd1c53c88c6b1
massiliasreign <masonvongsavath619@gmail.com>
2026-01-02 18:31:55 -0800

    arguments

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import sys
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+book_path = sys.argv[1]
 from stats import get_num_words, sorted_letter_count 
 print("============ BOOKBOT ============")
-print("Analyzing book found at books/frankenstein.txt...")
+print("Analyzing book found at books/...")
 print("----------- Word Count ----------")
 get_num_words()
 print("--------- Character Count -------")
--- stats.py
+++ stats.py
@@ -3,13 +3,11 @@ def get_book_text(filepath):
         file_contents = f.read()
         return file_contents
 def get_num_words():
-    book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     text = text.split()
     num_words = len(text)
     print(f"Found {num_words} total words")
 def letter_count():
-    book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     letter_counts = {}
     for letter in text:
